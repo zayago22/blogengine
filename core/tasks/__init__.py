bb6e8096efb2e4de86a018587d91008af6ba0b10
zayago22 <hola@rekobit.com>
2026-02-28 11:58:27 -0600

    Fix: import all Celery tasks in __init__.py for worker discovery

--- core/tasks/__init__.py
+++ core/tasks/__init__.py
@@ -1,1 +1,6 @@
 """BlogEngine - Tareas Celery."""
+from core.tasks.generation import generate_scheduled_posts, generate_single_article, generate_batch
+from core.tasks.publishing import auto_publish_scheduled, publish_single, unpublish_single
+from core.tasks.seo_ping import ping_all_clients, ping_client_sitemap
+from core.tasks.social import distribute_pending, generate_social_for_post, publish_social_post
+from core.tasks.calendar_gen import generate_calendars, generate_client_calendar
